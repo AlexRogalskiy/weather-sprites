8840c96934b1ee52d51b4b680d04a725d87d4e14
ADMSK\AVROGAL1 <avrogal1@mts.ru>
2021-05-15 00:25:01 +0300

    docs: updates on workflows
    Added information on documentation

--- core/src/colors.ts
+++ core/src/colors.ts
@@ -1,6 +1,73 @@
 import * as P from 'polished'
 
-export const defaultBreakpoints = [40, 52, 64].map((n: number): string => `${n}em`)
+import { compose } from 'ramda'
+
+import { Styles } from 'polished/lib/types/style'
+import { ContrastScores, HslaColor, HslColor, RgbaColor, RgbColor } from 'polished/lib/types/color'
+
+import { BiProcessor, Processor } from '../typings/standard-types'
+
+export const breakpoints = (args: number[] = [40, 52, 64]): string[] =>
+    args.map((n: number): string => `${n}em`)
+
+export const radialGradient: Styles = P.radialGradient({
+    colorStops: ['#00FFFF 0%', 'rgba(0, 0, 255, 0) 50%', '#0000FF 95%'],
+    extent: 'farthest-corner at 45px 45px',
+    position: 'center',
+    shape: 'ellipse',
+    fallback: '',
+})
+
+export const triangle = P.triangle({
+    backgroundColor: 'red',
+    foregroundColor: 'red',
+    pointingDirection: 'right',
+    width: 100,
+    height: 100,
+})
+
+export const linearGradient: Styles = P.linearGradient({
+    colorStops: ['#00FFFF 0%', 'rgba(0, 0, 255, 0) 50%', '#0000FF 95%'],
+    toDirection: 'to top right',
+    fallback: '#FFF',
+})
+
+export const backgroundImages: Styles = P.backgroundImages(
+    'url("/image/background.jpg")',
+    'linear-gradient(red, green)'
+)
+
+export const backgrounds: Styles = P.backgrounds(
+    'url("/image/background.jpg")',
+    'linear-gradient(red, green)',
+    'center no-repeat'
+)
+
+export const border: Styles = P.border('top', '1px', 'solid', 'red')
+export const borderColor: Styles = P.borderColor('red', null, undefined, 'green')
+export const borderRadius: Styles = P.borderRadius('top', '5px')
+export const borderStyle: Styles = P.borderStyle('solid', null, undefined, 'dashed')
+export const borderWidth: Styles = P.borderWidth('12px', null, undefined, '24px')
+export const buttons: string = P.buttons(null, undefined, 'active')
+export const margin: Styles = P.margin('12px', null, undefined, '24px')
+export const padding: Styles = P.padding('12px', null, undefined, '24px')
+
+export const position: Styles = P.position('absolute', '12px', null, undefined, '24px')
+
+export const textInputs: string = P.textInputs('active', null, undefined)
+export const transitions: Styles = P.transitions('opacity 1.0s ease-in 0s', 'width 2.0s ease-in 2s')
+
+export const directionalProperty: Styles = P.directionalProperty('padding', '12px', null, undefined, '24px')
+
+export const remToPx: string = P.remToPx('1.6rem', '10px')
+
+export const stripUnit: number | string = P.stripUnit('100px')
+
+export const tone = (a: number, b: number): Processor<string, string> =>
+    compose(P.lighten(a), P.desaturate(b))
+
+export const toneCompose = (c: string, a: number, b: number): Processor<Styles, string> =>
+    compose(P.lighten(a), desaturate(c, b))
 
 export const get = (obj: any, key: string, def?: unknown, p?: number, undef?: unknown): any => {
     const path: string[] = key ? key.split('.') : [key]
@@ -10,6 +77,177 @@ export const get = (obj: any, key: string, def?: unknown, p?: number, undef?: un
     return obj === undef ? def : obj
 }
 
+/**
+ * Returns color from theme.colors
+ */
+export const getColor = (style: Styles, color: string): string =>
+    get(style, `colors.${color}`, color)
+        .replace(/^var\(--(\w+)(.*?), /, '')
+        .replace(/\)/, '')
+
+/**
+ * Returns color string representation
+ */
+export const colorString = (v: any): string => P.toColorString(v)
+
+/**
+ * Returns between boundary value
+ */
+export const between = (a: string, b: string): Processor<Styles, string> => (t: Styles) =>
+    P.between(getColor(t, a), getColor(t, b))
+
+/**
+ * Returns darkened color
+ */
+export const darken = (c: string, n: number): Processor<Styles, string> => (t: Styles) =>
+    P.darken(n, getColor(t, c))
+
+/**
+ * Returns readable color
+ */
+export const readableColor = (c: string): Processor<Styles, string> => (t: Styles): string =>
+    P.readableColor(getColor(t, c))
+
+/**
+ * Returns HSL color by input string
+ */
+export const toHsl = (c: string): Processor<Styles, HslColor | HslaColor> => (t: Styles) =>
+    P.parseToHsl(getColor(t, c))
+
+/**
+ * Returns RGB color by input string
+ */
+export const toRgb = (c: string): Processor<Styles, RgbColor | RgbaColor> => (t: Styles) =>
+    P.parseToRgb(getColor(t, c))
+
+/**
+ * Returns color by HSLA
+ */
+export const hsla = (t: HslaColor): string => P.hsla(t)
+
+/**
+ * Returns color by HSL
+ */
+export const hsl = (t: HslColor): string => P.hsl(t)
+
+/**
+ * Returns opacified color by an amount 0–1
+ */
+export const opacify = (c: string, n: number): Processor<Styles, string> => (t: Styles) =>
+    P.opacify(n, getColor(t, c))
+
+/**
+ * Returns lightened color by an amount 0–1
+ */
+export const lighten = (c: string, n: number): Processor<Styles, string> => (t: Styles) =>
+    P.lighten(n, getColor(t, c))
+
+/**
+ * Returns color with rotated hue by an amount 0–360
+ */
+export const rotate = (c: string, d: number): Processor<Styles, string> => (t: Styles) =>
+    P.adjustHue(d, getColor(t, c))
+
+/**
+ * Returns color with hue to a degree 0–360
+ */
+export const hue = (c: string, h: number): Processor<Styles, string> => (t: Styles) =>
+    P.setHue(h, getColor(t, c))
+
+/**
+ * Returns color with saturation
+ */
+export const saturation = (c: string, s: number): Processor<Styles, string> => (t: Styles) =>
+    P.setSaturation(s, getColor(t, c))
+
+/**
+ * Returns color with lightness
+ */
+export const lightness = (c: string, l: number): Processor<Styles, string> => (t: Styles) =>
+    P.setLightness(l, getColor(t, c))
+
+/**
+ * Returns desaturated color
+ */
+export const desaturate = (c: string, n: number): Processor<Styles, string> => (t: Styles) =>
+    P.desaturate(n, getColor(t, c))
+
+/**
+ * Returns saturated color
+ */
+export const saturate = (c: string, n: number): Processor<Styles, string> => (t: Styles) =>
+    P.saturate(n, getColor(t, c))
+
+/**
+ * Returns shaded color
+ */
+export const shade = (c: string, n: number): Processor<Styles, string> => (t: Styles) =>
+    P.shade(n, getColor(t, c))
+
+/**
+ * Returns tinted color
+ */
+export const tint = (c: string, n: number): Processor<Styles, string> => (t: Styles) =>
+    P.tint(n, getColor(t, c))
+
+/**
+ * Returns transparent color
+ */
+export const transparentize = (c: string, n: number): Processor<Styles, string> => (t: Styles) =>
+    P.transparentize(n, getColor(t, c))
+
+/**
+ * Returns RGBA color
+ */
+export const alpha = (c: string, n: number): Processor<Styles, string> => (t: Styles) =>
+    P.rgba(getColor(t, c), n)
+
+/**
+ * Returns mix of two colors by a specific ratio
+ */
+export const meetsContrastGuidelines = (a: string, b: string): Processor<Styles, ContrastScores> => (
+    t: Styles
+) => P.meetsContrastGuidelines(getColor(t, a), getColor(t, b))
+
+/**
+ * Returns mix of two colors by a specific ratio
+ */
+export const mix = (a: string, b: string, n = 0.5): Processor<Styles, string> => (t: Styles) =>
+    P.mix(n, getColor(t, a), getColor(t, b))
+
+/**
+ * Returns complement of a color
+ */
+export const complement = (c: string): Processor<Styles, string> => (t: Styles) =>
+    P.complement(getColor(t, c))
+
+/**
+ * Returns inverted color
+ */
+export const invert = (c: string): Processor<Styles, string> => (t: Styles) => P.invert(getColor(t, c))
+
+/**
+ * Returns gray the color
+ */
+export const gray = (c: string): Processor<Styles, string> => (t: Styles) => P.grayscale(getColor(t, c))
+
+/**
+ * Returns luminance of a color
+ */
+export const contrast = (c: string): BiProcessor<Styles, Styles, number> => (t1: Styles, t2: Styles) =>
+    P.getContrast(getColor(t1, c), getColor(t2, c))
+
+/**
+ * Returns luminance of a color
+ */
+export const luminance = (c: string): Processor<Styles, number> => (t: Styles) =>
+    P.getLuminance(getColor(t, c))
+//--------------------------------------------------------------------------------------------------
+/**
+ * Returns desaturated (grayscale) color
+ */
+export const grayscale = (c: string): Processor<Styles, string> => desaturate(c, 1)
+
 // const styles = {
 //     'color': P.lighten(0.2, '#000'),
 //     'font-size': P.modularScale(1),
@@ -17,80 +255,3 @@ export const get = (obj: any, key: string, def?: unknown, p?: number, undef?: un
 //         'font-size': P.modularScale(1.25),
 //     },
 // }
-
-/**
- * Get color from theme.colors
- */
-export const getColor = (theme: any, color: string): string =>
-    get(theme, `colors.${color}`, color)
-        .replace(/^var\(--(\w+)(.*?), /, '')
-        .replace(/\)/, '')
-
-/**
- * Darken a color by an amount 0–1
- */
-export const darken = (c: string, n: number) => (t: any) => P.darken(n, getColor(t, c))
-/**
- * Lighten a color by an amount 0–1
- */
-export const lighten = (c: string, n: number) => (t: any) => P.lighten(n, getColor(t, c))
-/**
- * Rotate the hue of a color by an amount 0–360
- */
-export const rotate = (c: string, d: number) => (t: any) => P.adjustHue(d, getColor(t, c))
-
-/**
- * Set the hue of a color to a degree 0–360
- */
-export const hue = (c: string, h: number) => (t: any) => P.setHue(h, getColor(t, c))
-/**
- * Set the saturation of a color to an amount 0–1
- */
-export const saturation = (c: string, s: number) => (t: any) => P.setSaturation(s, getColor(t, c))
-/**
- * Set the lightness of a color to an amount 0–1
- */
-export const lightness = (c: string, l: number) => (t: any) => P.setLightness(l, getColor(t, c))
-/**
- * Desaturate a color by an amount 0–1
- */
-export const desaturate = (c: string, n: number) => (t: any) => P.desaturate(n, getColor(t, c))
-/**
- * Saturate a color by an amount 0–1
- */
-export const saturate = (c: string, n: number) => (t: any) => P.saturate(n, getColor(t, c))
-
-/**
- * Shade a color by an amount 0–1
- */
-export const shade = (c: string, n: number) => (t: any) => P.shade(n, getColor(t, c))
-/**
- * Tint a color by an amount 0–1
- */
-export const tint = (c: string, n: number) => (t: any) => P.tint(n, getColor(t, c))
-
-export const transparentize = (c: string, n: number) => (t: any) => P.transparentize(n, getColor(t, c))
-/**
- * Set the transparency of a color to an amount 0-1
- */
-export const alpha = (c: string, n: number) => (t: any) => P.rgba(getColor(t, c), n)
-
-/**
- * Mix two colors by a specific ratio
- */
-export const mix = (a: string, b: string, n = 0.5) => (t: any) => P.mix(n, getColor(t, a), getColor(t, b))
-
-/**
- * Get the complement of a color
- */
-export const complement = (c: string) => (t: any) => P.complement(getColor(t, c))
-
-/**
- * Get the inverted color
- */
-export const invert = (c: string) => (t: any) => P.invert(getColor(t, c))
-
-/**
- * Desaturate the color to grayscale
- */
-export const grayscale = (c: string): ((t: any) => string) => desaturate(c, 1)
--- core/typings/standard-types.ts
+++ core/typings/standard-types.ts
@@ -13,10 +13,34 @@ export type Optional<T> = T | null | undefined
 export type Processor<T, V> = (v: T) => V
 //--------------------------------------------------------------------------------------------------
 /**
+ * Supplier
+ * @desc Type representing supplier function type in TypeScript
+ * @example
+ *   type Supplier = () => return new String()
+ */
+export type Supplier<T> = () => T
+//--------------------------------------------------------------------------------------------------
+/**
+ * BiProcessor
+ * @desc Type representing binary processor function type in TypeScript
+ * @example
+ *   type BiProcessor = (v1, v2) => return new String(v1 + v2)
+ */
+export type BiProcessor<T, P, V> = (v1: T, v2: P) => V
+//--------------------------------------------------------------------------------------------------
+/**
  * BiConsumer
  * @desc Type representing binary consumer function type in TypeScript
  * @example
  *   type BiConsumer = (v1, v2) => console.log(v1 + ":" + v2)
  */
 export type BiConsumer<T, V> = (v1: T, v2: V) => void
+//--------------------------------------------------------------------------------------------------
+/**
+ * StyledRecord
+ * @desc Type representing styled record
+ */
+export type StyledRecord = {
+    [ruleOrSelector: string]: PropertyKey | StyledRecord
+}
 //--------------------------------------------------------------------------------------------------
