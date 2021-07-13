823e9ce51e6d3464f686498b7d886130be8b7483
AlexRogalskiy <a.rogalskii@bftcom.com>
2021-07-13 15:44:23 +0300

    Added info on workflows
    Updates on github-actions

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,11 @@
+const fs = require('fs');
+const path = require('path');
+
+const packages = fs.readdirSync(path.resolve(__dirname, 'api'));
+
 module.exports = {
     extends: ['@commitlint/config-conventional', '@commitlint/config-lerna-scopes'],
     rules: {
-        'scope-enum': [2, 'always', ['release']],
+        'scope-enum': [2, 'always', ['release', ...packages]],
     },
 }
